bd0d0b6433f5b9f975adba6ce1581f8caa33c550
Saramanda9988 <2074730050@qq.com>
2026-05-21 09:04:01 +0800

    fix: fix class generation in ToggleGroupItem component for biome check

--- src/components/ui/toggle-group/ToggleGroupItem.vue
+++ src/components/ui/toggle-group/ToggleGroupItem.vue
@@ -5,7 +5,7 @@ import type { ToggleGroupItemProps } from 'reka-ui'
 import { ToggleGroupItem, useForwardProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 import { inject } from 'vue'
-import type { toggleVariants } from '@/components/ui/toggle'
+import { toggleVariants } from '@/components/ui/toggle'
 import { cn } from '@/lib/utils'
 
 type ToggleGroupVariants = VariantProps<typeof toggleVariants> & {
@@ -24,6 +24,17 @@ const context = inject<ToggleGroupVariants>('toggleGroup')
 
 const delegatedProps = reactiveOmit(props, 'class', 'size', 'variant')
 const forwardedProps = useForwardProps(delegatedProps)
+
+function getToggleGroupItemClass() {
+  return cn(
+    'group-data-[spacing=0]/toggle-group:rounded-none group-data-[spacing=0]/toggle-group:px-2 group-data-[spacing=0]/toggle-group:has-data-[icon=inline-end]:pr-1.5 group-data-[spacing=0]/toggle-group:has-data-[icon=inline-start]:pl-1.5 group-data-horizontal/toggle-group:data-[spacing=0]:first:rounded-l-lg group-data-vertical/toggle-group:data-[spacing=0]:first:rounded-t-lg group-data-horizontal/toggle-group:data-[spacing=0]:last:rounded-r-lg group-data-vertical/toggle-group:data-[spacing=0]:last:rounded-b-lg shrink-0 focus:z-10 focus-visible:z-10 group-data-horizontal/toggle-group:data-[spacing=0]:data-[variant=outline]:border-l-0 group-data-vertical/toggle-group:data-[spacing=0]:data-[variant=outline]:border-t-0 group-data-horizontal/toggle-group:data-[spacing=0]:data-[variant=outline]:first:border-l group-data-vertical/toggle-group:data-[spacing=0]:data-[variant=outline]:first:border-t',
+    toggleVariants({
+      variant: context?.variant || props.variant,
+      size: context?.size || props.size,
+    }),
+    props.class
+  )
+}
 </script>
 
 <template>
@@ -34,13 +45,7 @@ const forwardedProps = useForwardProps(delegatedProps)
     :data-size="context?.size || size"
     :data-spacing="context?.spacing"
     v-bind="forwardedProps"
-    :class="cn(
-      'group-data-[spacing=0]/toggle-group:rounded-none group-data-[spacing=0]/toggle-group:px-2 group-data-[spacing=0]/toggle-group:has-data-[icon=inline-end]:pr-1.5 group-data-[spacing=0]/toggle-group:has-data-[icon=inline-start]:pl-1.5 group-data-horizontal/toggle-group:data-[spacing=0]:first:rounded-l-lg group-data-vertical/toggle-group:data-[spacing=0]:first:rounded-t-lg group-data-horizontal/toggle-group:data-[spacing=0]:last:rounded-r-lg group-data-vertical/toggle-group:data-[spacing=0]:last:rounded-b-lg shrink-0 focus:z-10 focus-visible:z-10 group-data-horizontal/toggle-group:data-[spacing=0]:data-[variant=outline]:border-l-0 group-data-vertical/toggle-group:data-[spacing=0]:data-[variant=outline]:border-t-0 group-data-horizontal/toggle-group:data-[spacing=0]:data-[variant=outline]:first:border-l group-data-vertical/toggle-group:data-[spacing=0]:data-[variant=outline]:first:border-t',
-      toggleVariants({
-        variant: context?.variant || variant,
-        size: context?.size || size,
-      }),
-      props.class)"
+    :class="getToggleGroupItemClass()"
   >
     <slot v-bind="slotProps" />
   </ToggleGroupItem>
