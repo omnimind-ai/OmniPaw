cebf3c2e99e35fc6fed780afcd9da186b9525289
Saramanda9988 <2074730050@qq.com>
2026-07-02 00:09:05 +0800

    refactor: update scrollbar styles and enhance sidebar appearance

--- src/components/ui/scroll-area/ScrollBar.vue
+++ src/components/ui/scroll-area/ScrollBar.vue
@@ -20,11 +20,11 @@ const delegatedProps = reactiveOmit(props, 'class')
     data-slot="scroll-area-scrollbar"
     :data-orientation="orientation"
     v-bind="delegatedProps"
-    :class="cn('data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-l data-vertical:border-l-transparent flex touch-none p-px transition-colors select-none', props.class)"
+    :class="cn('data-horizontal:h-1.5 data-horizontal:flex-col data-vertical:h-full data-vertical:w-1.5 flex touch-none transition-colors select-none', props.class)"
   >
     <ScrollAreaThumb
       data-slot="scroll-area-thumb"
-      class="rounded-full relative flex-1 bg-border"
+      class="rounded-full relative flex-1 bg-foreground/25 hover:bg-foreground/40 transition-colors"
     />
   </ScrollAreaScrollbar>
 </template>
